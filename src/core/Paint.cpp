8e74a598b3183edb01e5917a405abbd941b111a3
Huiyicc <1925473403@qq.com>
2024-06-17 18:29:57 +0800

    实验使用硬件加速

--- src/core/Paint.cpp
+++ src/core/Paint.cpp
@@ -6,8 +6,10 @@
 namespace HYGUI {
 
 void HYPaintSetColor(PaintPtr paint, HYARGB color) {
-  // 大多数情况下颜色空间使用的是BGRA,所以这里要注意一下
-  paint->setColor(SkColorSetARGB(color.a, color.b, color.g, color.r));
+  // 无硬件加速情况下颜色空间使用的是BGRA,所以这里要注意一下
+  // paint->setColor(SkColorSetARGB(color.a, color.b, color.g, color.r));
+  paint->setColor(SkColorSetARGB(color.a, color.r, color.g, color.b));
+
 }
 
 }
