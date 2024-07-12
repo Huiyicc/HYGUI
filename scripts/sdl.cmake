37e8ac088d88ecc79dac7a11bd9b75b4f96f063e
Huiyicc <1925473403@qq.com>
2024-07-12 20:56:35 +0800

    自动化sdl3,除了skia以外都能自动编译

--- scripts/sdl.cmake
+++ scripts/sdl.cmake
@@ -4,31 +4,24 @@ set(EP_SDL_DIR modules/projects/SDL)
 set(SDL_INSTALL_PATH ${PROJECT_SOURCE_DIR}/modules)
 set(SDL_BINARY_PATH ${CMAKE_BINARY_DIR}/submodules/SDL)
 
-#ExternalProject_Add(SDL_SUBMODEL
-#    SOURCE_DIR "${EP_SDL_DIR}"
-#    INSTALL_DIR "${SDL_INSTALL_PATH}"
-#    TMP_DIR "${SDL_BINARY_PATH}/tmp"
-#    BINARY_DIR "${SDL_BINARY_PATH}/bin"
-#)
-#
-#add_custom_target(SDL_SUBMODEL_BUILD ALL
-#    COMMAND ${CMAKE_COMMAND} --build ${SDL_BINARY_PATH}
-#    WORKING_DIRECTORY ${SDL_INSTALL_PATH}
-#)
-if (WIN32)
-  add_definitions(-DWIN32)
-  add_definitions(-D_WINDOWS)
+if (CMAKE_HOST_WIN32)
+  #  add_definitions(-DWIN32)
+  #  add_definitions(-D_WINDOWS)
   add_definitions(-DUSING_GENERATED_CONFIG_H)
   add_definitions(-D_CRT_SECURE_NO_DEPRECATE)
+  add_definitions(-D_NO_CRT_STDIO_INLINE)
   add_definitions(-D_CRT_NONSTDC_NO_DEPRECATE)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
   add_definitions(-DSDL_BUILD_MAJOR_VERSION=3)
   add_definitions(-DSDL_BUILD_MINOR_VERSION=1)
   add_definitions(-DSDL_BUILD_MICRO_VERSION=2)
-  add_definitions(-DCMAKE_INTDIR="${CMAKE_BUILD_TYPE}")
-  add_definitions(-DDLL_EXPORT)
-
+  #  add_definitions(-DCMAKE_INTDIR="${CMAKE_BUILD_TYPE}")
+  #  add_definitions(-DDLL_EXPORT)
+  set(WINDOWS_STORE OFF)
+  set(SDL_LIBC ON)
+  set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
 endif ()
 set(SDL_STATIC ON)
 set(SDL_SHARED OFF)
 add_subdirectory(${MODULES_DIR}/projects/SDL)
+include_directories(${MODULES_DIR}/projects/SDL/include)
